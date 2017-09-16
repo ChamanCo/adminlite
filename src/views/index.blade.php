733834c319aa362dc4803d160d19901d77314f20
Florian Moreno <fr.florianmoreno@gmail.com>
2017-09-16 05:59:34 +0200

    Update index.blade.php

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('adminlite::layouts.app')
 
 @section('content')
-    <div class="row">
+    <div class="row" style="">
         <div class="col-lg-3 col-md-6 col-sm-6">
             <div class="card card-stats">
                 <div class="card-header" data-background-color="orange">
